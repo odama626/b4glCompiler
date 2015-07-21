30a943b52b62ad380f012a3c904dbc198bb0546e
Omarzion <adamsparks92@gmail.com>
2015-07-21 17:17:59 -0500

    removed files that don't belong

--- bin/Debug/b4gl_ide/programs/errorTesting.asm
+++
@@ -1,7 +0,0 @@
-	global _start
-	
-	section .data
-	global_byte_buffer: DB 0
-	a:	DW 50
-	section .text
-_start:
--- bin/Debug/b4gl_ide/programs/fuckin.asm
+++
@@ -1,6 +0,0 @@
-	global _start
-	
-	section .data
-	global_byte_buffer: DB 0
-	section .text
-_start:
